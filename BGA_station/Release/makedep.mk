5269665ca5318a388f7c7c655dd958713b50be87
benchmarex <120053921+benchmarex@users.noreply.github.com>
2023-01-10 22:39:15 +0100

    Add files via upload

---
+++ BGA_station/Release/makedep.mk
@@ -0,0 +1,18 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+adc.c
+
+i2c_twi.c
+
+main.c
+
+pcd8544.c
+
+pid.c
+
+TWI_TC74_read.c
+
+_keyboard.c
+
